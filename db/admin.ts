a6ede5a247324fce82afdade031f010c8ac3c1bb
izruff <rafaelky.dev@gmail.com>
2025-05-21 00:35:06 +0800

    Remove NEXT_PUBLIC_ prefixes from database-related secrets
    NEXT_PUBLIC_ is only needed when the env variable is retrieved by client code.

--- db/admin.ts
+++ db/admin.ts
@@ -2,9 +2,9 @@ import { initializeApp, cert, getApps, App } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 const serviceAccount = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 let adminApp: App;
