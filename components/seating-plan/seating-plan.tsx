bb00dfde5a1e05da5a061f83db63815aa2be6ffd
izruff <rafaelky.dev@gmail.com>
2025-05-22 00:49:26 +0800

    Modify SeatingPlan to use Context

--- components/seating-plan/seating-plan.tsx
+++ components/seating-plan/seating-plan.tsx
@@ -1,18 +1,37 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import { BaseSeatingPlanManager, SeatSelectionResult } from "./types";
+
+export interface SeatingPlanContextType {
+  width: number,
+  height: number,
+  stageLocation: {
+    x: { start: number, end: number },
+    y: { start: number, end: number },
+  }
+
+  manager: BaseSeatingPlanManager<any>,
+  seatSelectionResultsHandler: (results: SeatSelectionResult[]) => Promise<void> | void,
+  seatContentBuilder: (id: string) => ReactNode,
+}
+
+export const SeatingPlanContext = React.createContext<SeatingPlanContextType | null>(null);
 
 interface SeatComponentProps {
   id: string,
-  manager: BaseSeatingPlanManager<any>,
-  resultHandler: (result: SeatSelectionResult) => Promise<void> | void,
   children: ReactNode,
 }
 
-export default function Seat({ id, manager, resultHandler, children }: SeatComponentProps) {
-  if (!manager._seatIds.has(id)) {
-    throw new Error("Seat not found");
+function Seat({ id, children }: SeatComponentProps) {
+  const context = React.useContext(SeatingPlanContext);
+  if (context === null) {
+    return (
+      <div>Loading...</div>
+    );
   }
-  
+
+  const manager = context.manager;
+  const resultHandler = (result: SeatSelectionResult) => context.seatSelectionResultsHandler([result]);
+
   const seatMetadata = manager.seatMap.get(id)!;
   const seatState = manager.seatStateMap.get(id)!;
 
@@ -48,24 +67,19 @@ export default function Seat({ id, manager, resultHandler, children }: SeatCompo
   );
 }
 
-interface SeatingPlanProps {
-  width: number,
-  height: number,
-  manager: BaseSeatingPlanManager<any>,
-  seatSelectionResultsHandler: (results: SeatSelectionResult[]) => Promise<void> | void,
-  seatContentBuilder: (id: string, manager: BaseSeatingPlanManager<any>) => ReactNode,
-}
-
-export function SeatingPlan({ width, height, manager, seatSelectionResultsHandler, seatContentBuilder }: SeatingPlanProps) {
+export function SeatingPlan() {
+  const context = React.useContext(SeatingPlanContext);
+  if (context === null) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+  
   return (
-    <div style={{ width, height }}>
-      {Array.from(manager.seatMap.keys()).map(id => (
-        <Seat
-          id={id}
-          manager={manager}
-          resultHandler={res => seatSelectionResultsHandler([res])}
-        >
-          {seatContentBuilder(id, manager)}
+    <div style={{ width: context.width, height: context.height }}>
+      {Array.from(context.manager.seatMap.keys()).map(id => (
+        <Seat id={id}>
+          {context.seatContentBuilder(id)}
         </Seat>
       ))}
     </div>
