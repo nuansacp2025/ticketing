8e9d9967304c5bffe1f41d81aac014ea643cedc3
izruff <rafaelky.dev@gmail.com>
2025-05-27 11:04:15 +0800

    Add style and transition to FancyButton

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -7,14 +7,18 @@ export const FancyButton: React.FC<{
   onClick: () => void,
 }> = ({ variant, sizeClass, children, onClick }) => {
   // Only white variant for now
+  // Animation: https://buttons.ibelick.com/
   return (
     <button
-      className={`${sizeClass} rounded-3xl bg-transparent outline-2 outline-foreground`}
+      className={`group relative overflow-hidden rounded-3xl bg-transparent outline-2 outline-foreground transition-colors duration-500 ${sizeClass}`}
       onClick={onClick}
     >
-      <div className="w-full text-center">
+      <span className="relative z-10 transition-colors duration-500 group-hover:text-background">
         {children}
-      </div>
+      </span>
+      <span className="absolute inset-0 overflow-hidden rounded-3xl pointer-events-none">
+        <span className="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-foreground transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span>
+      </span>
     </button>
   );
 };
