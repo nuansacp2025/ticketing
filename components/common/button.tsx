f42d2297a6e6813112446b1efd229b5e52042794
izruff <rafaelky.dev@gmail.com>
2025-05-28 00:03:39 +0800

    Edit and add more buttons

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -1,24 +1,59 @@
+// Animations credited to https://buttons.ibelick.com/
+
 import React, { ReactNode } from "react";
 
 export const FancyButton: React.FC<{
   variant: "white",
   sizeClass: string,
   children: ReactNode,
-  onClick: () => void,
+  onClick?: () => void,
 }> = ({ variant, sizeClass, children, onClick }) => {
   // Only white variant for now
-  // Animation: https://buttons.ibelick.com/
   return (
     <button
-      className={`group relative overflow-hidden rounded-3xl bg-transparent outline-2 outline-foreground transition-colors duration-500 ${sizeClass}`}
+      className={`cursor-pointer group relative overflow-hidden bg-transparent border-2 border-foreground transition-colors duration-500 ${sizeClass}`}
       onClick={onClick}
     >
-      <span className="relative z-10 transition-colors duration-500 group-hover:text-background">
+      <span className="relative z-10 transition-colors duration-500 text-foreground group-hover:text-background">
         {children}
       </span>
-      <span className="absolute inset-0 overflow-hidden rounded-3xl pointer-events-none">
+      <span className="absolute inset-0 overflow-hidden pointer-events-none">
         <span className="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-foreground transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span>
       </span>
     </button>
+  );
+};
+
+export const RegularButton: React.FC<{
+  variant: "black",
+  sizeClass: string,
+  children: ReactNode,
+  onClick?: () => void,
+}> = ({ variant, sizeClass, children, onClick }) => {
+  // Only black variant for now
+  return (
+    <button
+      className={`cursor-pointer relative bg-[#222222] group transition-all duration-300 active:scale-110 ${sizeClass}`}
+      onClick={onClick}
+    >
+      <span className="relative z-10 text-foreground">
+        {children}
+      </span>
+      <span className="absolute inset-0 overflow-hidden pointer-events-none bg-transparent group-hover:bg-white/10 duration-300" />
+    </button>
+  );
+};
+
+export const InlineButton: React.FC<{
+  children: string,
+  onClick?: () => void,
+}> = ({ children, onClick }) => {
+  return (
+    <span
+      className="underline underline-offset-2 decoration-1 hover:decoration-2 hover:animate-pulse"
+      onClick={onClick}
+    >
+      {children}
+    </span>
   );
 };
