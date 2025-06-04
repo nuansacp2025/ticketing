e4fa56c65806d61b7ea3aa84776efec824f970bd
izruff <rafaelky.dev@gmail.com>
2025-06-04 15:07:13 +0800

    Implement simple extract file API

--- api/index.py
+++ api/index.py
@@ -1,11 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
 
+import os
+import csv
+from time import time_ns
+from scripts.getfile.extract_file import extract_file
+
 @app.route("/api/python")
 def hello_world():
+    # Test if Vercel allows creating/writing/reading files
+    folder_name = f"attachments_{time_ns()}"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name, mode=0o777)
+    folder_path = os.path.abspath(folder_name)
+    files = extract_file(os.path.abspath(folder_path))
+
+    rows = []
+    for file in files:
+        with open(file, "r") as f:
+            content = csv.reader(f)
+            for row in content:
+                rows.append(",".join(row))
+
     return {
         "success": True,
-        "message": "Hello, World!"
+        "message": "\n".join(rows),
     }
 
 if __name__ == '__main__':
