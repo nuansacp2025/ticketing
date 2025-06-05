ef25b555848ccf258393d62cb6f2dabaa9a76962
Joseph Oliver <joseph.o.lim@gmail.com>
2025-06-06 00:51:04 +0800

    process orders

--- api/index.py
+++ api/index.py
@@ -1,8 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+from .scripts.processing import process_orders
+
 @app.route("/api/python")
 def hello_world():
+    process_orders()
     return {
         "success": True,
         "message": "Hello, World!"
