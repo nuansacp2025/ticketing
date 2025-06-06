eb1bd81a72fc95e0157c1aac31a92ac3bac52f38
izruff <rafaelky.dev@gmail.com>
2025-06-06 19:34:20 +0800

    Edit styles for thank-you page

--- app/(main)/thank-you/page.tsx
+++ app/(main)/thank-you/page.tsx
@@ -1,13 +1,22 @@
+import { RegularButton } from "@/components/common/button";
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <div className="flex flex-col items-center text-center">
-      <h2>You're all set!</h2>
-      <div>
-        <p>You will receive an email confirming your seat allocation. Show this email to our staff when you arrive at the venue.</p>
-        <p>Please check your spam folder as it may accidentally be flagged as spam.</p>
-        <p>We look forward to seeing you in the show!</p>
+    <div className="w-full max-w-[720px] min-h-2/3 m-8 p-8 sm:p-16 flex flex-col items-center justify-center rounded-2xl bg-[#EEEEEE] text-background">
+      <div className="w-full h-full flex flex-col items-center justify-between space-y-8 text-center overflow-x-hidden overflow-y-auto">
+        <h2 className="px-4 text-2xl sm:text-3xl font-medium">You're all set!</h2>
+        <div className="sm:px-4 flex flex-col items-center space-y-4 text-sm sm:text-base font-light">
+          <p>You will receive an email confirming your seat allocation. Show this email to our staff when you arrive at the venue.</p>
+          <p>Please check your spam folder as it may accidentally be flagged as spam.</p>
+          <p>We look forward to seeing you in the show!</p>
+        </div>
+        <Link href="/ticket" className="w-full">
+          <RegularButton variant="black" buttonClass="w-full max-w-[480px] h-[48px] rounded-3xl">
+            <span className="text-base sm:text-lg font-medium">Back to Home</span>
+          </RegularButton>
+        </Link>
       </div>
-      <button>Back to Home</button>
     </div>
   );
 }
