5d15dcdc3fe11d30ee8b3754ddbe8a2c3e23d777
izruff <rafaelky.dev@gmail.com>
2025-06-12 19:37:06 +0800

    Make fixes and update requirements

--- api/index.py
+++ api/index.py
@@ -8,6 +8,7 @@ API_CREDS_INTERNAL_USE_ONLY = os.getenv("API_CREDS_INTERNAL_USE_ONLY")
 from flask import Flask, request
 app = Flask(__name__)
 
+import aiohttp
 import asyncio
 from .scripts.mailgun.mailer import send_email
 from .scripts.processing import TicketPDFGenerator
@@ -31,7 +32,7 @@ async def send_seat_confirmation():
         profile = data.get("profile")
         email = profile.get("email")
         ticket_code = profile.get("ticketCode")
-        seats = data.get("seats", [])
+        seats = data.get("seats")
 
         assert isinstance(email, str)
         assert isinstance(ticket_code, str)
@@ -45,18 +46,21 @@ async def send_seat_confirmation():
 
     pdfGen = TicketPDFGenerator()
     attachments = pdfGen.generate_pdfs_from_seats([(s["label"], s["category"]) for s in seats])
+    attachments = [(fname, bytes_io.getbuffer()) for fname, bytes_io in attachments]
     try:
-        email_res = await send_email(
-            to_email=email,
-            subject="NUANSA 2025 Seat Confirmation",
-            template_name="seat_confirmation.html",
-            context={
-                "ticket_code": ticket_code,
-                "share_link": "https://tickets.nuansacp.org/share",
-                "seat_num": ", ".join(seats),
-            },
-            attachments=attachments,
-        )
+        async with aiohttp.ClientSession() as session:
+            email_res = await send_email(
+                session=session,
+                to_email=email,
+                subject="NUANSA 2025 Seat Confirmation",
+                template_name="seat_confirmation.html",
+                context={
+                    "ticket_code": ticket_code,
+                    "share_link": "https://tickets.nuansacp.org/share",
+                    "seat_num": ", ".join(f"{s['label']} ({s['category']})" for s in seats),
+                },
+                attachments=attachments,
+            )
         print(email_res)  # expect this to be 200 always
         return { "success": True }, 200
     except Exception as e:
