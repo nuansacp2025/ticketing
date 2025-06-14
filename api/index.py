6a3950d15307b7d84e0aa6923f8c2d5874052283
Joseph Oliver <joseph.o.lim@gmail.com>
2025-06-14 21:01:47 +0800

    Securing cron job

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 import os
 API_CREDS_INTERNAL_USE_ONLY = os.getenv("API_CREDS_INTERNAL_USE_ONLY")
+CRON_SECRET = os.getenv("CRON_SECRET")
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -15,6 +16,10 @@ from .scripts.processing import TicketPDFGenerator
 
 @app.route("/api/python")
 def hello_world():
+    auth_header = request.headers.get("authorization")
+    if auth_header != f"Bearer {CRON_SECRET}":
+        return {"success": False, "message": "Unauthorized"}, 401
+    
     return {
         "success": True,
         "message": "Hello, World!"
