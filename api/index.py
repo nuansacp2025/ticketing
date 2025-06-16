ccc8d629af39adcda57e7a0058b5129c812e859d
izruff <rafaelky.dev@gmail.com>
2025-06-16 23:58:04 +0800

    Add fix

--- api/index.py
+++ api/index.py
@@ -11,8 +11,7 @@ app = Flask(__name__)
 
 import aiohttp
 import asyncio
-from .scripts.mailgun.mailer import send_email
-from .scripts.processing import TicketPDFGenerator
+from .scripts import send_seat_confirmation, TicketPDFGenerator, get_ticket_ref
 
 @app.route("/api/python")
 def hello_world():
@@ -26,50 +25,26 @@ def hello_world():
     }
 
 @app.route("/api/email/sendSeatConfirmation", methods=["POST"])
-async def send_seat_confirmation():
+async def handle_seat_confirmation():
     # TODO: IMPORTANT! Need to check if this bypass is secure or not
     if request.headers["X-Internal-API-Credentials"] != API_CREDS_INTERNAL_USE_ONLY:
         return {"success": False, "message": "Unauthorized"}, 401
 
     try:
         data = request.get_json()
-        print(data)
-        profile = data.get("profile")
-        email = profile.get("email")
-        ticket_code = profile.get("ticketCode")
-        seats = data.get("seats")
-
-        assert isinstance(email, str)
+        ticket_code = data.get("ticketCode")
         assert isinstance(ticket_code, str)
-        assert isinstance(seats, list)
-        for seat in seats:
-            assert isinstance(seat, dict)
-            assert "label" in seat and "category" in seat
     except Exception as e:
-        print(repr(e))
-        return { "success": False, "message": "Fields `email`, `ticketCode`, and `seats` required" }, 400
+        return { "success": False, "message": "Field `ticketCode` required" }, 400
 
-    pdfGen = TicketPDFGenerator()
-    attachments = pdfGen.generate_pdfs_from_seats([(s["label"], s["category"]) for s in seats])
-    attachments = [(fname, bytes_io.getbuffer()) for fname, bytes_io in attachments]
+    ticket_ref = get_ticket_ref(ticket_code)
+    pdf_generator = TicketPDFGenerator()
+
     try:
         async with aiohttp.ClientSession() as session:
-            email_res = await send_email(
-                session=session,
-                to_email=email,
-                subject="NUANSA 2025 Seat Confirmation",
-                template_name="seat_confirmation.html",
-                context={
-                    "ticket_code": ticket_code,
-                    "share_link": "https://tickets.nuansacp.org/share",
-                    "seat_num": ", ".join(f"{s['label']} ({s['category']})" for s in seats),
-                },
-                attachments=attachments,
-            )
-        print(email_res)  # expect this to be 200 always
+            await send_seat_confirmation(session, ticket_ref, pdf_generator)
         return { "success": True }, 200
     except Exception as e:
-        print(repr(e))
         return { "success": False, "message": str(e) }, 502
 
 if __name__ == '__main__':
