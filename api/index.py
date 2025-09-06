c8d9d87c7c9b1931fc2dd6a399828872a4b79d5f
izruff <rafaelky.dev@gmail.com>
2025-09-06 20:38:28 +0800

    Remove `sendSeatConfirmation` endpoint

--- api/index.py
+++ api/index.py
@@ -9,12 +9,6 @@ CRON_SECRET = os.getenv("CRON_SECRET")
 from flask import Flask, request
 app = Flask(__name__)
 
-import aiohttp
-import asyncio
-from .scripts.mailgun.mailer import send_seat_confirmation
-from .scripts.processing import TicketPDFGenerator
-from .scripts.db.db import db
-
 @app.route("/api/python")
 def hello_world():
     auth_header = request.headers.get("authorization")
@@ -25,30 +19,6 @@ def hello_world():
         "success": True,
         "message": "Hello, World!"
     }
-
-@app.route("/api/email/sendSeatConfirmation", methods=["POST"])
-async def handle_seat_confirmation():
-    # TODO: IMPORTANT! Need to check if this bypass is secure or not
-    if request.headers["X-Internal-API-Credentials"] != API_CREDS_INTERNAL_USE_ONLY:
-        return {"success": False, "message": "Unauthorized"}, 401
-
-    try:
-        data = request.get_json()
-        ticket_id = data.get("ticketId")
-        assert isinstance(ticket_id, str)
-    except Exception as e:
-        return { "success": False, "message": "Field `ticketCode` required" }, 400
-
-    # TODO: this should be in scripts/db/db.py instead
-    ticket_ref = db.document("tickets", ticket_id)
-    pdf_generator = TicketPDFGenerator()
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            await send_seat_confirmation(session, ticket_ref, pdf_generator)
-        return { "success": True }, 200
-    except Exception as e:
-        return { "success": False, "message": str(e) }, 502
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8000, debug=True)
